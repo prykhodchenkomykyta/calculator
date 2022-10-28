56d7bba135c8fdb196f81250aa75a61d62971d01
Mykyta Prykhodchenko <nikitaprihodchenco@ukr.net>
2022-10-29 01:26:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+let a = '';
+let b = '';
+let sign = '';
+let finish = false;
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', '*', '/'];
+
+const out = document.querySelector('.calc__screen p');
+
+function clearAll() {
+	a = '';
+	b = '';
+	sign = '';
+	finish = false;
+	out.textContent = 0;
+}
+
+document.querySelector('.button__ac').onclick = clearAll;
+
+document.querySelector('.calc__buttons').onclick = (event) => {
+	if(!event.target.classList.contains('button')) return;
+	if(event.target.classList.contains('button__ac')) return;
+
+	out.textContent = '';
+
+	const key = event.target.textContent;
+
+	if (digit.includes(key)) {
+		if (b === '' && sign === '') {
+			a += key;
+			out.textContent = a;
+		} 
+		else if (a!== '' && b!== '' && finish) {
+			b = key;
+			finish = false;
+			out.textContent = b;
+		} else {
+			b += key;
+			out.textContent = b;
+		}
+					console.log(a, b, sign);
+					return;
+	}
+
+		if (action.includes(key)) {
+		sign = key;
+		console.log(sign);
+		out.textContent = sign;
+		return;
+	}
+
+	if (key === '=') {
+		if (b === '') b = a;
+		switch(sign) {
+			case '+':
+					a = (+a) + (+b);
+					break;
+			case '-':
+					a = a - b;
+					break;
+			case '*':
+					a = a * b;
+					break;
+			case '/':
+				if (b === '0') {
+					out.textContent = 'Error';
+					a = '';
+					b = '';
+					sign = '';
+					return;
+				}
+					a = a / b;
+					break;
+		}
+		finish = true;
+		out.textContent = a;
+		console.log(a, b, sign);
+	}
+
+}
